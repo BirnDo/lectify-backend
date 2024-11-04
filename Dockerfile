ffa219f6c1e16d31e8789975e5e62d3976b502a1
Dominik Birngruber <d.birngruber2002@gmail.com>
2024-11-04 19:57:17 +0100

    initial project commit

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-FROM hello-world
+FROM continuumio/miniconda3
+
+COPY app app
+COPY env.yml env.yml
+RUN conda env create --file env.yml -n lectify-backend && conda init
+SHELL ["conda", "run", "-n", "lectify-backend", "/bin/bash", "-c"]
+
+ENTRYPOINT ["conda", "run", "--no-capture-output", "-n", "lectify-backend", "python", "app/app.py"]
---
+++ app/app.py
@@ -0,0 +1,24 @@
+import json
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/health')
+def health():
+    return 'Healthy!'
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    return request.get_json().get('message')
+
+
+if __name__ == '__main__':
+    app.run()
