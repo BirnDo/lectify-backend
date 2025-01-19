aaf3cd95a8b98663890c0283d300e0b94965d701
Dominik Birngruber <d.birngruber2002@gmail.com>
2025-01-19 20:35:04 +0100

    update test, env and workflow

--- app/tests/test_example.py
+++ app/tests/test_example.py
@@ -7,24 +7,10 @@ import time
 # Add the parent directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Import the Flask app instance from the main app file
-from app import app
 # Import pytest for writing and running tests
 import pytest
 
-@pytest.fixture
-def client():
-    """A test client for the app."""
-    with app.test_client() as client:
-        yield client
-
-def test_non_existent_route(client):
-    """Test for a non-existent route."""
-    response = client.get('/non-existent')
-    assert response.status_code == 404
-
-
-def test_dummy(client):
+def test_dummy():
     """run a simple dummy test"""
     time.sleep(5)
     assert 1+1 == 2
